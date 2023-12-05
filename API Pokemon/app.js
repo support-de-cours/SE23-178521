54812c8233ca19e5594b38282d3de99350fe0f2b
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-12-05 17:13:37 +0100

    -

--- API Pokemon/app.js
+++ API Pokemon/app.js
@@ -5,76 +5,148 @@ const target = document.querySelector('[rel=js-pokemons]');
 
 
 
-// <li><a href="#">xxxx</a></li>
 
-// const pokeName = "Pika";
+async function httpGet(uri)
+{
+    const response = await fetch(uri);
+    const data = await response.json();
 
-// let node_a = document.createElement('a');
-//     node_a.href = "#poke";
-//     node_a.innerText = pokeName;
-
-// let node_span = document.createElement('span');
-//     node_span.innerHTML = " <strong>+ d'infos</strong>";
-
-// let node_li = document.createElement('li');
-//     node_li.append(node_a);
-//     node_li.append(node_span);
-
-// target.append(node_li);
+    return data;
+}
 
 
 
+function showPokemons(pokemons)
+{
+    Array.from(pokemons, pokemon => {
 
-// Fetch va chercher des données stockées sur l'URL
-fetch(url)
+        let node_a = document.createElement('a');
+            node_a.href = "#poke";
+            node_a.innerText = pokemon.name;
 
-    // Quand fetch reçois les données, on les conveties en JSON
-    .then( response => response.json() )
+        let node_span = document.createElement('span');
+            node_span.innerHTML = " <strong>+ d'infos</strong>";
 
-    // "data" = les données en JSON
-    .then( data => { 
+        let node_li = document.createElement('li');
+            node_li.append(node_a);
+            node_li.append(node_span);
 
-        // Filtre le JSON pour recup la propriété "results" fournie par l'API
-        const pokemons = data.results;
-
-        // Boucle sur la liste des pokemon stockée dans "results" de l'API
-        Array.from(pokemons, pokemon => {
-
-            // Injection HTML de la balise
-            // <li><a href="#">nom du pokemon</a></li>
-            // --
-            // 1. Inject de la balise <li> dans la balise <ol id="pokemons">
-            // 2. Création de la balise <li>
-            // 2.a et 2.b inject les balise <a> et <span> dans la balise <li>
-            // 3. Creation de la balise <span>
-            // 4. Creation de la balise <a>
-            // 4.a Ajout de l'attribut "href" sur la balis <a>
-            // 4.b Ajout du texte dans la balis <a>
-
-            // 4.
-            let node_a = document.createElement('a');
-                // 4.a
-                node_a.href = "#poke";
-                // 4.b
-                node_a.innerText = pokemon.name;
-
-            // 3.
-            let node_span = document.createElement('span');
-                node_span.innerHTML = " <strong>+ d'infos</strong>";
-
-            // 2.
-            let node_li = document.createElement('li');
-                // 2.a
-                node_li.append(node_a);
-                // 2.b
-                node_li.append(node_span);
-
-            // 1.
-            target.append(node_li);
-
-        })
+        target.append(node_li);
     })
-    .catch( error => console.error(error) )
-;
+}
+
+
+(async function(){
+    let pokemons = await httpGet(url);
+
+    showPokemons(pokemons.results);
+})();
+
+
+
+
+
+
+
+
+
+
+
+// 
+
+
+// fetch(url)
+//     .then( response => response.json() )
+//     .then( data => { 
+
+//         const pokemons = data.results;
+//         Array.from(pokemons, pokemon => {
+
+//             let node_a = document.createElement('a');
+//                 node_a.href = "#poke";
+//                 node_a.innerText = pokemon.name;
+
+//             let node_span = document.createElement('span');
+//                 node_span.innerHTML = " <strong>+ d'infos</strong>";
+
+//             let node_li = document.createElement('li');
+//                 node_li.append(node_a);
+//                 node_li.append(node_span);
+
+//             target.append(node_li);
+//         })
+//     })
+//     .catch( error => console.error(error) )
+// ;
+
+
+
+
+// const response = await fetch(url);
+// console.log(response);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Fetch va chercher des données stockées sur l'URL
+// fetch(url)
+
+//     // Quand fetch reçois les données, on les conveties en JSON
+//     .then( response => response.json() )
+
+//     // "data" = les données en JSON
+//     .then( data => { 
+
+//         // Filtre le JSON pour recup la propriété "results" fournie par l'API
+//         const pokemons = data.results;
+
+//         // Boucle sur la liste des pokemon stockée dans "results" de l'API
+//         Array.from(pokemons, pokemon => {
+
+//             // Injection HTML de la balise
+//             // <li><a href="#">nom du pokemon</a></li>
+//             // --
+//             // 1. Inject de la balise <li> dans la balise <ol id="pokemons">
+//             // 2. Création de la balise <li>
+//             // 2.a et 2.b inject les balise <a> et <span> dans la balise <li>
+//             // 3. Creation de la balise <span>
+//             // 4. Creation de la balise <a>
+//             // 4.a Ajout de l'attribut "href" sur la balis <a>
+//             // 4.b Ajout du texte dans la balis <a>
+
+//             // 4.
+//             let node_a = document.createElement('a');
+//                 // 4.a
+//                 node_a.href = "#poke";
+//                 // 4.b
+//                 node_a.innerText = pokemon.name;
+
+//             // 3.
+//             let node_span = document.createElement('span');
+//                 node_span.innerHTML = " <strong>+ d'infos</strong>";
+
+//             // 2.
+//             let node_li = document.createElement('li');
+//                 // 2.a
+//                 node_li.append(node_a);
+//                 // 2.b
+//                 node_li.append(node_span);
+
+//             // 1.
+//             target.append(node_li);
+
+//         })
+//     })
+//     .catch( error => console.error(error) )
+// ;
 
 console.log( url );
